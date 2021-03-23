bb56c103ea504b7d3620380388d6cfc39231a68c
Tyler Farris <Tylerpfarris@gmail.com>
2021-03-23 15:24:09 -0700

    filter passing

--- LAB/findIndex.js
+++ LAB/findIndex.js
@@ -1,5 +1,14 @@
-const findIndex = () => {
 
-}
+const findIndex = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        const bool = callback(arr[i])
+        if (bool === true) {
+            return i;
+        };
+    };
+    return -1;
+};
 
 module.exports = findIndex;
+
+
--- LAB/findIndex.test.js
+++ LAB/findIndex.test.js
@@ -1,8 +1,10 @@
+const findIndex = require("./findIndex");
 
 describe('findIndex function', () => {
     it('returns the index of the found item, `-1` if no item is found', () => {
         const fruits = ['apple', 'banana', 'orange', 'strawberry'];
         expect(findIndex(fruits, (el) => el === 'orange')).toEqual(2)
+        expect(findIndex(fruits, (el) => el === 'peach')).toEqual(-1)
         
     })
 })
